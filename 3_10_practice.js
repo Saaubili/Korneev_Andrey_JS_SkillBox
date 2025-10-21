68fbd6275622a9e542e69939a7d125b64099102c
Saaubili <sarikaubili@gmail.com>
2025-10-21 14:11:15 +0500

    Add files via upload

---
+++ 3_10_practice.js
@@ -0,0 +1,10 @@
+const cardInserted = true;
+const balance = 500;
+const amount = 1000;
+
+if (cardInserted && amount <= balance){
+  console.log("Операция выполняется")
+}
+else{
+  console.log("Операция отклонена")
+}
